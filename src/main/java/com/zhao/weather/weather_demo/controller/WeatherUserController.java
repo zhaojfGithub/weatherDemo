ea9cbc51e41a613cec2230a9c1712df78775e839
zjf <zjfGithub@163.com>
2021-03-30 09:17:41 +0800

    登录，注册完成

--- src/main/java/com/zhao/weather/weather_demo/controller/WeatherUserController.java
+++ src/main/java/com/zhao/weather/weather_demo/controller/WeatherUserController.java
@@ -4,6 +4,7 @@ package com.zhao.weather.weather_demo.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zhao.weather.weather_demo.entity.WeatherUser;
 import com.zhao.weather.weather_demo.service.WeatherUserService;
+import com.zhao.weather.weather_demo.utils.RestResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +12,7 @@ import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author zhao
@@ -24,47 +25,48 @@ public class WeatherUserController {
     @Autowired
     private WeatherUserService weatherUserService;
 
-    @GetMapping("findAll")
-    public List<WeatherUser> finaAllTeacher(){
-        List<WeatherUser> list=weatherUserService.list(null);
-        return list;
-    }
-
-    @GetMapping("addOneData")
-    public boolean addOneData(){
-        WeatherUser user = new WeatherUser();
-        user.setName("key1");
-        user.setEmail("toy111111@163.com");
-        user.setAccountNumber(111111111L);
-        user.setPassword("toy111111");
-        return weatherUserService.save(user);
-    }
-
-    @PostMapping("login")
-    public String login(@RequestParam("name") String name,
-                         @RequestParam("accountNumber") Long accountNumber,
-                         @RequestParam("password") String password,
-                         @RequestParam("email") String email){
-        WeatherUser user = new WeatherUser();
-        user.setName(name);
-        user.setEmail(email);
-        user.setPassword(password);
-        user.setAccountNumber(accountNumber);
-        if(weatherUserService.save(user)){
-            return "注册成功";
+    /**
+     * 注册
+     * @param accountNumber 账号
+     * @param password      密码
+     */
+    @PostMapping("register")
+    public RestResponse<Object> register(@RequestParam("accountNumber") Long accountNumber,
+                                         @RequestParam("password") String password) {
+        QueryWrapper<WeatherUser> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("account_number", accountNumber);
+        List<WeatherUser> list = weatherUserService.list(queryWrapper);
+        if (list.size() == 0) {
+            WeatherUser user = new WeatherUser();
+            user.setPassword(password);
+            user.setAccountNumber(accountNumber);
+            if (weatherUserService.save(user)) {
+                return RestResponse.success("注册成功");
+            } else {
+                return RestResponse.fail("注册失败，请联系开发者");
+            }
         }else {
-            return "注册失败" ;
+            return RestResponse.fail("注册失败，此手机号已经被使用");
         }
     }
 
-    @GetMapping("register")
-    public List<WeatherUser> register(@RequestParam("accountNumber") Long accountNumber,
-                         @RequestParam("password") String password){
-
+    /**
+     * 登录
+     *
+     * @param accountNumber 账号
+     * @param password      密码
+     * @return 返回用户id
+     */
+    @PostMapping("login")
+    public RestResponse<Object> login(@RequestParam("accountNumber") Long accountNumber,
+                                      @RequestParam("password") String password) {
         QueryWrapper<WeatherUser> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("account_number",accountNumber).eq("password",password);
-        List<WeatherUser> list=weatherUserService.list(queryWrapper);
-        return list;
+        queryWrapper.eq("account_number", accountNumber).eq("password", password);
+        List<WeatherUser> list = weatherUserService.list(queryWrapper);
+        if (list.size()==0){
+            return RestResponse.fail("登录失败,账号或密码错误");
+        }
+        return RestResponse.success("登录成功", list);
     }
 
 }
